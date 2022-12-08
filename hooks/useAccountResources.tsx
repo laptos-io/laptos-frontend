97c6251f7d0599ad7ece8999a8f186b72eb7dff8
akira <wrapped.akira@gmail.com>
2022-12-08 19:05:44 +0800

    add hooks for creating pools

--- hooks/useAccountResources.tsx
+++ hooks/useAccountResources.tsx
@@ -1,15 +1,41 @@
-import { useCallback } from "react";
+import { MaybeHexString } from "aptos";
+import { MoveResource } from "aptos/src/generated";
+import useSWR from "swr";
 
 import useAptosClient from "./useAptosClient";
-import useAptosWallet from "./useAptosWallet";
 
-function useAccountResources() {
-  const { connected, activeWallet } = useAptosWallet();
+export default function useAccountResources(accountAddress?: MaybeHexString) {
   const aptosClient = useAptosClient();
 
-  const walletAddress = activeWallet?.toString();
+  return useSWR<MoveResource[], unknown>(
+    accountAddress ? accountAddress : undefined,
+    () => aptosClient.getAccountResources(accountAddress!),
+    { refreshInterval: 0 }
+  );
+  // const [pending, setPending] = useState(false);
+  // const [data, setData] = useState<MoveResource[]>();
+  // const [error, setError] = useState<unknown>();
 
-  const getData = useCallback(async () => {
-    const resources = await aptosClient.getAccountResources(walletAddress!);
-  }, [aptosClient, walletAddress]);
+  // const getAccountResources = useCallback(async () => {
+  //   if (pending || !accountAddress) return null;
+  //   try {
+  //     const resources = await aptosClient.getAccountResources(accountAddress!);
+  //     setData(resources);
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  //   setPending(false);
+  // }, [accountAddress, aptosClient, pending]);
+
+  // useEffect(() => {
+  //   getAccountResources();
+  // }, [getAccountResources]);
+
+  // return useMemo(() => {
+  //   return {
+  //     pending,
+  //     data,
+  //     error,
+  //   };
+  // }, [data, error, pending]);
 }
