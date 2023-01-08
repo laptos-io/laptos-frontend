619a7632684edf7b8ec11b6812346dc0672dcc35
akira <wrapped.akira@gmail.com>
2023-01-09 01:50:09 +0800

    update header bg when scrolling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useWindowScroll } from "react-use";
+import { useRecoilValue } from "recoil";
+
+import { networkState } from "@/recoil/network";
 
 import Account from "./Account";
 import SearchBox from "./SearchBox";
 
 export default function Header() {
+  const { network } = useRecoilValue(networkState);
+  const { y } = useWindowScroll();
   return (
-    <header className="fixed inset-x-0 top-0 z-20 flex h-20 items-center justify-between bg-white px-8 py-0 shadow">
+    <header
+      className={`flex fixed inset-x-0 top-0 z-20 h-20 items-center justify-between px-8 py-0 ${
+        y && y > 80 ? "bg-white shadow" : "bg-transparent"
+      }`}
+    >
       <div className="inline-flex items-center justify-start space-x-3">
-        <Link href="/">
-          <Image width={120} height={40} src="/logo-full.svg" alt="logo" />
+        <Link passHref href={`/?network=${network}`}>
+          <a>
+            <Image width={120} height={40} src="/logo-full.svg" alt="logo" />
+          </a>
         </Link>
 
         <SearchBox className="hidden sm:block sm:w-48 lg:w-60 2xl:w-80" />
 
-        <Link passHref href="/collections">
+        <Link passHref href={`/collections?network=${network}`}>
           <a className="rounded-lg bg-primary/20 px-3 py-2 font-medium leading-6 text-primary transition-colors hover:bg-primary/30">
             Collections
           </a>
@@ -22,12 +34,12 @@ export default function Header() {
       </div>
 
       <div className="inline-flex items-center justify-start space-x-3">
-        <Link passHref href="/nfts">
+        <Link passHref href={`/nfts?network=${network}`}>
           <a className="rounded-lg bg-primary/20 px-3 py-2 font-medium leading-6 text-primary transition-colors hover:bg-primary/30">
             Your NFTs
           </a>
         </Link>
-        <Link passHref href="/pools">
+        <Link passHref href={`/pools?network=${network}`}>
           <a className="rounded-lg bg-primary/20 px-3 py-2 font-medium leading-6 text-primary transition-colors hover:bg-primary/30">
             Your Pools
           </a>
